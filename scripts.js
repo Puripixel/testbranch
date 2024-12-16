22a97676261fec5ce61825c9093522e597a7b73c
Puripixel <fzfybiz@gmail.com>
2024-12-16 09:32:08 +0700

    new test_V3

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
-console.log("Puripat")
-console.log("Puri")
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    alert('Invalid calculation');
+    clearDisplay();
+  }
+}
+
+function clearDisplay() {
+  display.value = '';
+}
